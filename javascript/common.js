17e703b50aca9b0a49efc515e470e48f8cbe425b
sowonHan <rippang781@gmail.com>
2022-04-13 19:05:35 +0900

    2022.04.13 모바일메뉴 삽질 중

---
+++ javascript/common.js
@@ -0,0 +1,17 @@
+/* 메뉴 버튼 변환 */
+
+$(function() {
+    $(document).ready(function() {
+        $('.trigger').click(function() {
+            $(this).toggleClass('open')
+        });
+    });
+});
+
+/* 모바일 메뉴 출력 */
+
+$(function() {
+    $(".trigger").click(function() {
+        $()
+    });
+});
