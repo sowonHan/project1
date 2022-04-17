b1c516681da7bdcd736db8da430ee30f14a1cb0a
sowonHan <rippang781@gmail.com>
2022-04-18 00:01:58 +0900

    2022.04.17

--- javascript/common.js
+++ javascript/common.js
@@ -15,3 +15,36 @@ $(function() {
         }
     });
 });
+
+/* resize 과부하 방지 코드 */
+
+(function() {
+    var throttle = function(type, name, obj) {
+        obj = obj || window;
+        var running = false;
+        var func = function() {
+            if (running) { return; }
+            running = true;
+            requestAnimationFrame(function() {
+                obj.dispatchEvent(new CustomEvent(name));
+                running = false;
+            });
+        };
+        obj.addEventListener(type, func);
+    };
+
+    throttle("resize", "optimizedResize");
+})();
+
+/* width가 768px 이상일 때 모바일 메뉴 보이는 것을 방지 */
+
+$(function() {
+    window.addEventListener('optimizedResize', () => {
+        let winWidth = window.innerWidth;
+    
+        if (winWidth >= 768) {
+            $(".mobile-menu").hide();
+            $(".trigger").removeClass('open');
+        }
+    });
+});
