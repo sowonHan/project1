c817553cf50a385fb711f7af0464bf2859340a8d
sowonHan <rippang781@gmail.com>
2023-05-02 21:47:57 +0900

    2023.05.02 readme 작성 완료

--- javascript/all_time.js
+++ javascript/all_time.js
@@ -1,26 +1,29 @@
 /* 풀페이지 스크롤 */
 
-new fullpage('#fullpage', {
-    //options here
-    autoScrolling: true,
-    responsiveWidth: 0, //픽셀로 정의된 폭 아래에 정상 스크롤(autoScrolling:false)을 씁니다.
-    credits: {
-        enabled: false
-        // default {enabled: true, label: 'Made with fullpage.js', position: 'right'}
-    }
+new fullpage("#fullpage", {
+  //options here
+  autoScrolling: true,
+  responsiveWidth: 0,
+  credits: {
+    enabled: false,
+    // default {enabled: true, label: 'Made with fullpage.js', position: 'right'}
+  },
 });
 
 /* 풀스크린 슬라이드 */
 
 $(function () {
-    $(".back-1").backstretch([
-        "../images/changgyeong_all.jpg",
-        "../images/deoksu_all.jpg"
-    ], { duration: 3000, fade: 750 });
+  $(".back-1").backstretch(
+    ["../images/changgyeong_all.jpg", "../images/deoksu_all.jpg"],
+    { duration: 3000, fade: 750 }
+  );
 
-    $(".back-3").backstretch([
-        "../images/seokjojeon1.jpg",
-        "../images/seokjojeon2.jpg",
-        "../images/seokjojeon3.jpg"
-    ], { duration: 3000, fade: 750 });
+  $(".back-3").backstretch(
+    [
+      "../images/seokjojeon1.jpg",
+      "../images/seokjojeon2.jpg",
+      "../images/seokjojeon3.jpg",
+    ],
+    { duration: 3000, fade: 750 }
+  );
 });
