ed7342ea4f8c6630d0a4f8ec47810921e3d6e8a8
sowonHan <rippang781@gmail.com>
2023-08-18 18:27:42 +0900

    2023.08.18 모바일메뉴 jquery 애니메이션 추가

--- javascript/common.js
+++ javascript/common.js
@@ -1,58 +1,60 @@
 /* 버튼 변환 및 모바일 메뉴 출력 */
 
-$(function() {
-    $(".trigger").click(function() {
-        $(this).toggleClass('open');
+$(function () {
+  $(".trigger").click(function () {
+    $(this).toggleClass("open");
 
-        if ($(this).hasClass("open")) {
-            $(".mobile-menu").show();
-            $(".trigger-container").addClass("shadow");
-            $("header").removeClass("transparent");
-        } else {
-            $(".mobile-menu").hide();
-            $(".trigger-container").removeClass("shadow");
-            $("header").addClass("transparent");
-        }
-    });
+    if ($(this).hasClass("open")) {
+      $(".mobile-menu__list").show(600);
+      $(".mobile-menu").show().animate({ width: "100%" }, 600);
+      $(".trigger-container").addClass("shadow");
+      //   $("header").removeClass("transparent");
+    } else {
+      $(".mobile-menu__list").hide();
+      $(".mobile-menu").animate({ width: 0 }, 600);
+      $(".trigger-container").removeClass("shadow");
+    }
+  });
 });
 
 /* resize 과부하 방지 코드 */
 
-(function() {
-    var throttle = function(type, name, obj) {
-        obj = obj || window;
-        var running = false;
-        var func = function() {
-            if (running) { return; }
-            running = true;
-            requestAnimationFrame(function() {
-                obj.dispatchEvent(new CustomEvent(name));
-                running = false;
-            });
-        };
-        obj.addEventListener(type, func);
+(function () {
+  var throttle = function (type, name, obj) {
+    obj = obj || window;
+    var running = false;
+    var func = function () {
+      if (running) {
+        return;
+      }
+      running = true;
+      requestAnimationFrame(function () {
+        obj.dispatchEvent(new CustomEvent(name));
+        running = false;
+      });
     };
+    obj.addEventListener(type, func);
+  };
 
-    throttle("resize", "optimizedResize");
+  throttle("resize", "optimizedResize");
 })();
 
 /* width가 768px 이상일 때 모바일 메뉴 보이는 것을 방지 */
 
-$(function() {
-    window.addEventListener('optimizedResize', () => {
-        let winWidth = window.innerWidth;
-    
-        if (winWidth >= 768) {
-            $(".mobile-menu").hide();
-            $(".trigger").removeClass('open');
-        }
-    });
-});
+$(function () {
+  window.addEventListener("optimizedResize", () => {
+    let winWidth = window.innerWidth;
 
+    if (winWidth >= 768) {
+      $(".mobile-menu").hide();
+      $(".trigger").removeClass("open");
+    }
+  });
+});
 
 /* div에 링크 걸기 */
 
 const openInNewTab = (url) => {
-    let win = window.open(url, '_blank');
-    win.focus();
-}
+  let win = window.open(url, "_blank");
+  win.focus();
+};
